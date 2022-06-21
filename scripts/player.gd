57485497e16ab6ad28007842a48fd1ac3779560a
kauecdev <kaue_cavalcante.cnt@outlook.com>
2022-06-21 20:45:13 -0300

    mod: changed jump mechanic, changed animation to animationplayer node, added animationtree to handle animation states

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,11 @@
 extends KinematicBody2D
 
-onready var sprite = get_node("Sprite")
 onready var particles = get_node("JetpackParticles")
 onready var bulletspawn = get_node("BulletSpawn")
 onready var sacredEffect = get_node("SacredEffect")
+onready var state_machine = $AnimationTree.get("parameters/playback")
+
 const bulletPath = preload("res://scenes/Bullet.tscn")
-onready var initial_scale = scale
 
 const ACCELERATION = 512
 const MAX_SPEED = 320
@@ -15,7 +15,7 @@ const JUMP_FORCE = 700
 
 var motion = Vector2.ZERO
 
-var did_first_jump = false
+var can_double_jump = false
 
 var is_attacking = false
 
@@ -27,13 +27,15 @@ var is_moving = false
 
 var is_normal_bullet = true
 
+
+func _ready():
+	$AnimationTree.get("parameters/playback")
+
 func _physics_process(delta):
 	
 	player_move(delta)
 	
 	detect_turn_around()
-	
-	play_jump_animation()
 	
 	player_attack()
 	
@@ -43,12 +45,15 @@ func _physics_process(delta):
 
 
 func player_move(delta):
+	if is_on_floor():
+		particles.emitting = false
+	
 	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
 	
 	if x_input != 0:
 		is_moving = true
 		if not is_attacking or not is_shooting:
-			sprite.play("Run")
+			state_machine.travel("Run")
 		motion.x += x_input * ACCELERATION
 		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
 		bulletspawn.position.y = -15
@@ -60,12 +65,11 @@ func player_move(delta):
 			is_moving_left = true
 			bulletspawn.position.x = -15
 			particles.position.x = 14
-#		sprite.flip_h = x_input < 0
 	else:
 		is_moving = false
 		bulletspawn.position.y = -20
-		if ((not is_attacking ) or (not is_shooting)):
-			sprite.play("Idle")
+		if not is_attacking or not is_shooting:
+			state_machine.travel("Idle")
 
 			
 		motion.x = lerp(motion.x, 0, FRICTION)
@@ -78,50 +82,46 @@ func player_move(delta):
 
 
 func player_jump():
-	if Input.is_action_just_released("ui_accept"):
-		if did_first_jump == false and not is_on_floor():
-			did_first_jump = true
-		particles.emitting = false
-			
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept"):
+		
 		if is_on_floor():
-			did_first_jump = false
+			can_double_jump = true
 			motion.y = -JUMP_FORCE
 			
-		if did_first_jump == true and not is_on_floor():
-			motion.y = -JUMP_FORCE
+		if not is_on_floor() and can_double_jump and Input.is_action_just_pressed("ui_accept"):
 			particles.emitting = true
-
+			motion.y = -JUMP_FORCE
+			can_double_jump = false
+		
+		play_jump_animation()
+		
 
 func play_jump_animation():
 	if not is_on_floor():
 		if not is_attacking:
-			sprite.play("Jump")
+			state_machine.travel("Jump")
+	else:
+		particles.emitting = false
 
 
 func detect_turn_around():
-	$Sprite.scale.x = (-1 if is_moving_left else 1)
+	$SpriteAnimation.flip_h = true if is_moving_left else false
+	$SacredEffect.position.x = (8 if is_moving_left else 0)
 	$PlayerCollisionShape.position.x = (8 if is_moving_left else 0)
 	
 
 func player_attack():
-	if Input.is_action_just_pressed("left_click") :
-		sprite.play("Attack")
-		is_attacking = true
+	if Input.is_action_just_pressed("left_click"):
+		state_machine.travel("Attack")
 		
 
-func _on_Sprite_animation_finished():
-	if sprite.animation == "Attack":
-		is_attacking = false
-	if sprite.animation == "IdleAttack":
-		is_shooting = false
 
 func player_shoot(delta):
 	# tecla z atira
 	if Input.is_action_just_pressed("shoot"):
 		is_shooting = true
 		if not is_moving:
-			sprite.play("IdleAttack")
+			state_machine.travel("IdleAttack")
 		
 		var bullet = bulletPath.instance()
 		get_parent().add_child(bullet)
@@ -143,3 +143,4 @@ func change_type_bullet():
 	if Input.is_action_just_pressed("change_type_bullet"):
 		is_normal_bullet = !is_normal_bullet
 		sacredEffect.visible = !is_normal_bullet
+
