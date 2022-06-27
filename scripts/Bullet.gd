56158ac410030b2f42f5e4e274e4e01bd43d133a
kauecdev <kaue_cavalcante.cnt@outlook.com>
2022-06-27 06:31:18 -0300

    feat: configured layer change and damage

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -4,23 +4,32 @@ extends Area2D
 var velocity = Vector2();
 var speed = 400;
 var is_normal = true
+export var sacred_water_damage = 15
 onready var sprite = get_node("sprite")
 # var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_collision_layer_bit(5, true)
+	set_collision_layer_bit(6, false)
 
 
 func _physics_process(delta):
 	if is_normal:
 		sprite.play("normal")
+		set_collision_layer_bit(5, true)
+		set_collision_layer_bit(6, false)
 	else:
 		sprite.play("sacred")
+		set_collision_layer_bit(5, false)
+		set_collision_layer_bit(6, true)
 	translate(velocity)
-
-
+	
 
 func _on_Bullet_body_entered(body):
+	queue_free()
+
+
+func _on_Bullet_area_entered(area):
 	queue_free()
