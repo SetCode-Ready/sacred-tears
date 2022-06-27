dab19765bac8df95ab5914ebb258738999a2078d
Filipe Cardoso <filipecardosooliveira@hotmail.com>
2022-06-27 16:18:35 -0300

    feat: add regeneration life in saint

--- scripts/saint.gd
+++ scripts/saint.gd
@@ -2,16 +2,32 @@ extends Area2D
 
 var player
 var in_saint = false
+export var life_saint = 50
+var regen_pressed = false
+onready var status_bar = $life_progres_saint
 
+
+
+func _process(delta):
+	status_bar.value = life_saint
+	
+	if Input.is_action_pressed("ui_regen") and player.life < 100 and life_saint > 0 and in_saint:
+		player.life += 0.125
+		life_saint -= 0.125
 
 func _on_saint_body_entered(body):
 	if body.name.to_upper() == 'PLAYER':
 		player = body
 		in_saint = true
+		if life_saint > 0:
+			$info_button.visible = true
+			$life_button.visible = true
 		$timer.start()
 
 
 func _on_saint_body_exited(body):
+	$info_button.visible = false
+	$life_button.visible = false
 	in_saint = false
 
 
@@ -19,3 +35,5 @@ func _on_timer_timeout():
 	if player.normal_water > 0 and player.sacred_water < 100 and in_saint:
 		player.sacred_water += 1 
 		player.normal_water -= 1
+
+
